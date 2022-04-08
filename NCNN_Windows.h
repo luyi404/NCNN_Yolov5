6493d33e5eb54784ae1d5e6a0f34191b90b42aa3
luyi <luyi@leinao.ai>
2022-04-08 11:19:06 +0800

    Modified CMakeList.txt to make it build correctly.

--- NCNN_Windows.h
+++ NCNN_Windows.h
@@ -1,8 +1,14 @@
 ﻿// NCNN_Windows.h: 标准系统包含文件的包含文件
 // 或项目特定的包含文件。
 
-#pragma once
-
 #include <iostream>
-
-// TODO: 在此处引用程序需要的其他标头。
+#include <opencv2/opencv.hpp>
+#include <map>
+#include <vector>  
+#include <algorithm>  
+#include <functional>  
+#include <cstdlib> 
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <net.h>
